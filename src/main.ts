f0a3c12d623aec09076043e7c3f9ed7e9cb6bc49
100Roads <93051531+100Roads@users.noreply.github.com>
2023-08-28 15:44:34 +0200

    28/08 update

--- src/main.ts
+++ src/main.ts
@@ -12,31 +12,37 @@ WA.onInit().then(() => {
     console.log('Player tags: ',WA.player.tags)
 
     // Julia custom
-
-    WA.room.onEnterLayer("floor").subscribe(() => {
-        WA.room.hideLayer("roof");
-        WA.room.hideLayer("walls-bg-front");
-        WA.room.hideLayer("sign");
+    WA.room.onEnterLayer("floor1").subscribe(() => {
+        WA.room.hideLayer("roof1");
+        WA.room.hideLayer("walls-bg-front1");
+        WA.room.hideLayer("sign1");
       });
-      
-    WA.room.onLeaveLayer("floor").subscribe(() => {
-        WA.room.showLayer("roof");
-        WA.room.showLayer("walls-bg-front");
-        WA.room.showLayer("facade-furniture-bg");
-        WA.room.showLayer("sign");
+    WA.room.onLeaveLayer("floor1").subscribe(() => {
+        WA.room.showLayer("roof1");
+        WA.room.showLayer("walls-bg-front1");
+        WA.room.showLayer("sign1");
       });
-
-      WA.room.onEnterLayer("rooms_floor").subscribe(() => {
+    WA.room.onEnterLayer("floor2").subscribe(() => {
+        WA.room.hideLayer("roof2");
+        WA.room.hideLayer("walls-bg-front2");
+        WA.room.hideLayer("sign2");
+      });
+    WA.room.onLeaveLayer("floor2").subscribe(() => {
+        WA.room.showLayer("roof2");
+        WA.room.showLayer("walls-bg-front2");
+        WA.room.showLayer("sign2");
+      });
+    WA.room.onEnterLayer("rooms_floor").subscribe(() => {
+        WA.room.hideLayer("facade-furniture-bg");
         WA.room.hideLayer("facade-furniture-fg");
         WA.room.hideLayer("facade");
-        WA.room.hideLayer("facade-furniture-bg");
       });
-      
     WA.room.onLeaveLayer("rooms_floor").subscribe(() => {
+        WA.room.showLayer("facade-furniture-bg");
         WA.room.showLayer("facade-furniture-fg");
         WA.room.showLayer("facade");
-        WA.room.showLayer("facade-furniture-bg");
       });
+      
     
     //Popup Liberary
     WA.room.onEnterLayer('message-1').subscribe(() => {
